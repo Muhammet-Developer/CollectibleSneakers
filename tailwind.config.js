a650e05a9336c9ca6e3615e99cf1ef80f2ff39a3
muhammet çetinkaya <muhammet@assatekno.com>
2023-07-26 08:49:06 +0300

    completed

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      xs: { max: '394px' },
+      sm: { max: '640px' },
+      md: { max: '768px' },
+      lg: { max: '1024px' },
+      xl: { max: '1280px' },
+      '2xl': { max: '1536px' },
+    },
+  },
+  plugins: [],
+};
